44508e769ef607c1aa6f3cdafe41f02e6bda7a9e
“[Nicholas <n.samaras4@outlook.com>
2023-01-10 14:20:18 -0500

    Users.js update

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import UsersList from '../components/UsersList'
 
+
 const Users = () => {
   const USERS = [
     { 
       id: 'ul', 
-      name: 'Max Schwartz', 
+      name: 'Nicholas Samaras', 
       image: 'https://avatars.githubusercontent.com/u/85946038?s=400&u=cc2db6c99dcdabcfd1f946cc2816e4d74b5d36e1&v=4', 
       places: 3 
     }
