1c6e5880509f91a1aa2fbf38406e1e68914beab1
“[Nicholas <n.samaras4@outlook.com>
2023-01-10 18:22:39 -0500

    App.js dot

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
-import Users from './user/pages/Users'
-import NewPlace from './places/pages/NewPlace'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+  Now,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
-  return <Router>
-    <Switch>
-    <Route path='/' exact>
-      <Users />
-    </Route>
-    <Route path='/places/new' exact>
-      <NewPlace />
-    </Route>
-    <Redirect to='/' />
-    </Switch>
-  </Router>
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
