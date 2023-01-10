7f3fea1195b39df8af367fda7b68bd159d7e18b7
“[Nicholas <n.samaras4@outlook.com>
2023-01-10 09:07:37 -0500

    Users.js update

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -2,7 +2,16 @@ import React from 'react'
 import UsersList from '../components/UsersList'
 
 const Users = () => {
-  return <UsersList />
+  const USERS = [
+    { 
+      id: 'ul', 
+      name: 'Max Schwartz', 
+      image: 'https://avatars.githubusercontent.com/u/85946038?s=400&u=cc2db6c99dcdabcfd1f946cc2816e4d74b5d36e1&v=4', 
+      places: 3 
+    }
+  ]
+
+  return <UsersList items={USERS} />
 }
 
 export default Users
