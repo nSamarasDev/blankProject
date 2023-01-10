b95874e243d4dafcc3c5b13fd7295dcbc2cc67d5
“[Nicholas <n.samaras4@outlook.com>
2023-01-10 09:06:22 -0500

    UserList.js update

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import './UserItem.css'
 
-const UsersItem = props => {
-
+const UserItem = props => {
+    return (
+        <li className='user-item'>
+            <div className='user-item_content'>
+                <div className='user-item_image'>
+                    <img src={props.image} alt={props.image} />
+                </div>
+                <div className='user-item_info'>
+                    <h2>{props.name}</h2>
+                    <h3>
+                        {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+                    </h3>
+                </div>
+            </div>
+        </li>
+    )
 }
 
-export default UsersItem
+export default UserItem
