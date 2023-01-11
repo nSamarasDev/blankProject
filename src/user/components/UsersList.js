dd6eac10bcf2d446a47b1d2f6bc798c6e06c6add
“[Nicholas <n.samaras4@outlook.com>
2023-01-11 12:09:05 -0500

    UsersList.js update

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,26 +1,35 @@
-import React from 'react'
-import './UsersList.css'
-import UserItem from './UserItem'
+import React from "react";
 
-const UsersList = props => {
-    if (props.items.length === 0) {
+import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+import "./UsersList.css";
+
+const UsersList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>No users found</h2>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => {
         return (
-            <div className='center'>
-                <h2>No users found</h2>
-            </div>
-        )
-    }
-
-    return <ul className='users-list'>
-        {props.items.map(user => {
-            return <UserItem 
-            key={user.id} 
-            id={user.id} 
-            image={user.image} 
-            name={user.name} 
-            placeCount={user.place} />
-        })}
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.place}
+          />
+        );
+      })}
     </ul>
-}
+  );
+};
 
-export default UsersList
+export default UsersList;
