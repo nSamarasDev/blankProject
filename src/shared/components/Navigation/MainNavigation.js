1bad17607290b63e7cfbb165ca673fc633972b5d
“[Nicholas <n.samaras4@outlook.com>
2023-01-12 14:28:35 -0500

    MainNavigation.js update

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -35,7 +35,7 @@ const MainNavigation = (props) => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">YourPlaces</Link>
+          <Link to="/">Places</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
