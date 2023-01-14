54a08bb28947f3ed0cf92cda86715aeb57243933
“[Nicholas <n.samaras4@outlook.com>
2023-01-14 10:42:26 -0500

    PlaceItem.js update

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
 import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
@@ -17,9 +18,9 @@ const PlaceItem = (props) => {
           <p>{props.creatorId}</p>
         </div>
         <div className="place-item__actions">
-          <button>View on map</button>
-          <button>Edit</button>
-          <button>Delete</button>
+          <Button inverse>View on map</Button>
+          <Button to={`/places/${props.id}`}>Edit</Button>
+          <Button danger>Delete</Button>
         </div>
       </Card>
     </li>
